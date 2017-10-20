16c52be2c736774ab8ceb8d99d706de97612c04f
jimchen5209 <jimchen5209@users.noreply.github.com>
2017-10-20 22:20:39 +0800

    rename .json to .py

--- langs/list.py
+++ langs/list.py
@@ -1,10 +1,10 @@
 {
   "zh_TW": {
-    "file": "./langs/zh_TW.json",
+    "file": "./langs/zh_TW.py",
     "display": "繁體中文（台灣）"
   },
   "en_US": {
-    "file": "./langs/en_US.json",
+    "file": "./langs/en_US.py",
     "display": "English(US)"
   }
 }
